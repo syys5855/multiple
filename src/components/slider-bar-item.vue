b8df3cc8d712b0bf46501bb3db05183ebdacc9e4
syys <1028824533@qq.com>
2017-10-17 15:45:25 +0800

    cebianlan

---
+++ src/components/slider-bar-item.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row slider-bar-item">
+        <div class="pull-left">
+            <span>{{barItemText}}</span>
+        </div>
+        <div class="pull-right">
+            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'barItemText'
+    ]
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/common.scss';
+.slider-bar {
+    &-item {
+        @extend .no-margin-hor;
+        @extend .padding-hor-15;
+        @extend .font-md;
+        height: 50px;
+        line-height: 50px;
+        background-color: #232a30;
+    }
+}
+</style>
